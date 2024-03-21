9b658ad60051340a085314c175d8e16414d4c477
pc <svx327@naver.com>
2024-03-21 15:27:10 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+
+import { getEmotionImage } from "./util/get-emotion-image";
+
+const App = () => {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <img src={getEmotionImage(1)} alt="" />
+        <img src={getEmotionImage(2)} alt="" />
+        <img src={getEmotionImage(3)} alt="" />
+        <img src={getEmotionImage(4)} alt="" />
+        <img src={getEmotionImage(5)} alt="" />
+      </div>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>new</Link>
+        <Link to={"/diary"}>diary</Link>
+      </div>
+      <button onClick={onClickButton}>new 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
